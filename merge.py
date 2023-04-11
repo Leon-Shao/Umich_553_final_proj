ff2702cf7f48d930ac92046b7efdccb9b71f4aa3
Yifan <spikedyf@umich.edu>
2023-04-11 11:39:51 -0400

    Data Generation Update

--- merge.py
+++ merge.py
@@ -4,10 +4,16 @@ import numpy as np
 
 fg_path = 'Foreground/'
 bg_path = 'Background/'
+gt_path = 'Trimap/'
+output_path = 'Dataset/'
 
+def read_picture_name(path):
+    picture_list = os.listdir(path)
+    return picture_list
 
-def composite4(fg, bg, a, w, h):
+def composite4(fg, bg, gt, a, w, h):
     fg = cv2.resize(fg, (w, h))
+    gt = cv2.resize(gt, (w, h))
     fg = np.array(fg, np.float32)
     bg_h, bg_w = bg.shape[:2]
     x = 0
@@ -22,7 +28,7 @@ def composite4(fg, bg, a, w, h):
     alpha = fg != 0
     im = alpha * fg + (1 - alpha) * bg
     im = im.astype(np.uint8)
-    return im, a, fg, bg
+    return im, a, fg, bg, gt
 
 def pic_padding(pic, w, h, x, y):
     pic_h, pic_w, c = pic.shape
@@ -30,16 +36,29 @@ def pic_padding(pic, w, h, x, y):
     im[x:x+pic_h, y:y+pic_w, :] = pic
     return im
 
-fg_pic = os.path.join(fg_path, '2007_000032.png')
-bg_pic = os.path.join(bg_path, '5_new_bg.png')
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
 
-fg = cv2.imread(fg_pic)
-bg = cv2.imread(bg_pic)
+fg_list = read_picture_name(fg_path)
+bg_list = read_picture_name(bg_path)
 
-a = 128
-
-h, w, c = bg.shape
-
-im, a, fg, bg = composite4(fg, bg, a, w, h)
-
-cv2.imwrite('test.png', im)
+count = 0
+for fg_name in fg_list:
+    for bg_name in bg_list:
+        fg_pic = os.path.join(fg_path, fg_name)
+        bg_pic = os.path.join(bg_path, bg_name)
+        tr_pic = os.path.join(gt_path, fg_name)
+        fg = cv2.imread(fg_pic)
+        bg = cv2.imread(bg_pic)
+        gt = cv2.imread(tr_pic)
+        a = 128
+        if type(bg) != type(None):
+            h, w, c = bg.shape
+            im, a, fg, bg, gt = composite4(fg, bg, gt, a, w, h)
+            pic_name = str(count) + '.png'
+            tri_name = str(count) + '_tri.png'
+            data_path = os.path.join(output_path, pic_name)
+            tri_path = os.path.join(output_path, tri_name)
+            cv2.imwrite(data_path, im)
+            cv2.imwrite(tri_path, gt)
+            count += 1
