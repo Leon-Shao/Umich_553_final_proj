e82a44ccda919cb4ebc96c4424061b4ad9d0a502
zhangtc <zhangtc@umich.edu>
2023-04-12 14:32:12 -0400

    add the evaluation

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset
 from model import Matting
 import torch.nn as nn 
 import torch.optim as optim
+from torch.utils.data import DataLoader
 
 class MattingDataset(Dataset):
     def __init__(self, img_dir, trimap_dir, alpha_dir, transfrom=None):
@@ -26,26 +27,41 @@ class MattingDataset(Dataset):
         alpha = Image.open(alpha_path).convert('L')
 
         return image, trimap, alpha
+if __name__ == "__main__":
+    train_img_dir = None
+    train_trimap_dir = None
+    train_alpha_dir = None
+    eval_img_dir = None
+    eval_trimap_dir = None
+    eval_alpha_dir = None
 
-train_img_dir = None
-train_trimap_dir = None
-train_alpha_dir = None
-eval_img_dir = None
-eval_trimap_dir = None
-eval_alpha_dir = None
+    train_dataset = MattingDataset(train_img_dir, train_trimap_dir, train_alpha_dir)
+    eval_dataset = MattingDataset(eval_img_dir, eval_trimap_dir, eval_alpha_dir)
 
-train_dataset = MattingDataset(train_img_dir, train_trimap_dir, train_alpha_dir)
-eval_dataset = MattingDataset(eval_img_dir, eval_trimap_dir, eval_alpha_dir)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = Matting().to(device)
-criterion = nn.L1Loss()
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
+    eval_loader = DataLoader(eval_dataset, batch_size=8, shuffle=True, num_workers=4)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = Matting().to(device)
+    criterion = nn.L1Loss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-num_epochs = 100
+    num_epochs = 100
 
-for epoch in range(num_epochs):
-    model.train()
-    running_loss = 0.0
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
 
-    for i, (images, trimaps, alphas) in enumerate(train_loader):
-        images 
+        for i, (images, trimaps, alphas) in enumerate(train_loader):
+            images = images.to(device)
+            trimaps = trimaps.to(device)
+            alphas = alphas.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(torch.cat([images, trimaps], dim=1))
+            loss = criterion(outputs, alphas)
+            loss.backward()
+
+            optimizer.step()
+            running_loss+=loss.item()
+            
